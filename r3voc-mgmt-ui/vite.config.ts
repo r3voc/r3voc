a61463be18f61d8737a9565536231e5a82c9e1c1
CommanderRedYT <ccommanderred@gmail.com>
2025-09-07 19:11:08 +0200

    Formatting

--- r3voc-mgmt-ui/vite.config.ts
+++ r3voc-mgmt-ui/vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // activate source maps in development
-  build: {
-    sourcemap: true,
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
+    plugins: [react()],
+    // activate source maps in development
+    build: {
+        sourcemap: true,
     },
-  },
-  preview: {
-    allowedHosts: true,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
+    server: {
+        proxy: {
+            '/api': 'http://localhost:3000',
+        },
     },
-  }
+    preview: {
+        allowedHosts: true,
+    },
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+    }
 })
